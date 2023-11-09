c6950108e834bf7e7a6c4a7dcb87984a482d2aef
AVSK's Lappy <kishore13071996@gmail.com>
2023-11-09 19:08:09 +0530

    Devops return statement

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World Test\n";
+        return "Hello World Team Devops\n";
     }
     
 }
